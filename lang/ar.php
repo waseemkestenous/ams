a194a12424177084e5392a7d52d5bc61bd583ba8
waseem <waseem sabry>
2022-09-06 17:17:25 +0200

    add ar language file

---
+++ lang/ar.php
@@ -0,0 +1,20 @@
+<?php
+define('_login','تسجيل الدخول');
+define('_error_login','ناسف, لايمكنك الدخول الي اللوحة الادارة');
+define('_username','اسم المستخدم');
+define('_pass','كلمة السر');
+define('_enter_username','ادخل اسم المستخدم');
+define('_enter_pass','ادخل كلمة المرور');
+define('_poweredby','ادارة وتطوير شركة <a href="https://meltonsoft.com">ميلتون سوفت</a>');
+define('_welcome','مرحباً');
+define('_home','الرئيسية');
+define('_settings','الاعدادات');
+define('_fullscreen','تكبير الشاشة');
+define('_lock','تعليق');
+define('_logout','تسجيل خروج');
+define('_profile','الملف الشخصي');
+define('_help','المساعدة');
+define('_see_all_alerts','مشاهدة جميع التنبيهات');
+
+
+
