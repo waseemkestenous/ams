c62685d221e252be8fb75992d4aafa3bb568fa6c
waseem <waseem.kestenous@gmail.com>
2023-05-28 12:47:30 +0300

    add dept and holiday to emp module

--- modules/emps/basic_lang/en.php
+++ modules/emps/basic_lang/en.php
@@ -2,5 +2,13 @@
 define('_emps_managment','Employees Managment');
 define('_emps','Employees Info.');
 define('_jobs','Jobs');
+define('_depts','Depts');
+define('_holidays','Holidays');
 
 define('_employees','Employees');
+
+// tables name
+define('jobs','Jobs');
+define('emps','Employees');
+define('depts','Departements');
+define('holidays','Holidays');
